bae5ab79bb46814730d9784f801953b59bdf35a1
H-a-ruuu <143mm@naver.com>
2026-07-30 02:45:38 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+window.SEONGUN_SHARED_DATA = {
+  "images": [
+    {
+      "id": "img_1785345179237",
+      "data": "https://raw.githubusercontent.com/H-a-ruuu/mp3/refs/heads/main/%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8256%202.jpeg",
+      "nick": ""
+    }
+  ],
+  "records": [
+    {
+      "id": "rec_1785345571464",
+      "title": "Light Me Up",
+      "desc": "",
+      "url": "https://www.postype.com/@ne3u14/post/22807552",
+      "author": "광목 커미션 ©findanotherkill"
+    }
+  ]
+};
